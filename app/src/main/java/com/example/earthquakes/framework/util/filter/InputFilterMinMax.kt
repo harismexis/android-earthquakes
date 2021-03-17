59c7becbace598ca89f29ba757b0e010b03500a8
Haris Mexis <theoharismexis@hotmail.com>
2021-03-17 02:05:42 +0000

    fixed prefs issues

---
+++ app/src/main/java/com/example/earthquakes/framework/util/filter/InputFilterMinMax.kt
@@ -0,0 +1,49 @@
+package com.example.earthquakes.framework.util.filter
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class InputFilterMinMax : InputFilter {
+    private var min: Int
+    private var max: Int
+
+    constructor(min: Int, max: Int) {
+        this.min = min
+        this.max = max
+    }
+
+    constructor(min: String, max: String) {
+        this.min = min.toInt()
+        this.max = max.toInt()
+    }
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence? {
+        try {
+            var newVal = dest.toString().substring(0, dstart) +
+                    dest.toString().substring(dend, dest.toString().length)
+
+            newVal = newVal.substring(0, dstart) +
+                    source.toString() +
+                    newVal.substring(dstart, newVal.length)
+
+            val input = newVal.toInt()
+
+            if (isInRange(min, max, input)) return null
+
+        } catch (nfe: NumberFormatException) {
+            nfe.printStackTrace()
+        }
+        return ""
+    }
+
+    private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+        return if (b > a) c in a..b else c in b..a
+    }
+}
--- app/src/main/java/com/example/earthquakes/presentation/preferences/PrefsFragment.kt
+++ app/src/main/java/com/example/earthquakes/presentation/preferences/PrefsFragment.kt
@@ -1,13 +1,29 @@
 package com.example.earthquakes.presentation.preferences
 
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.earthquakes.R
+import com.example.earthquakes.framework.extensions.makeSignedDecimal
+import com.example.earthquakes.framework.util.filter.InputFilterMinMax
 
 class PrefsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        (findPreference("north") as EditTextPreference?).makeSignedDecimal()
+        (findPreference("south") as EditTextPreference?).makeSignedDecimal()
+        (findPreference("east") as EditTextPreference?).makeSignedDecimal()
+        (findPreference("west") as EditTextPreference?).makeSignedDecimal()
+
+        val maxResults = findPreference("max_quake_results") as EditTextPreference?
+        maxResults?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_NUMBER
+            editText.filters = arrayOf<InputFilter>(InputFilterMinMax("1", "500"))
+        }
     }
 
 }
